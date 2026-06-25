316bde45cdee8d0512e818a9635dbfb08f605d06
Gaoyang <hangaoyang@me.com>
2026-06-25 16:32:33 +0800

    Add WinFsp prerequisite check on startup and sort disks by mount point
    - Check WinFsp 2.1.x via registry on startup; prompt user to download if missing
    - Remove winfsp.net NuGet package (replaced by registry detection)
    - Sort disk list by mount point when adding via AddDiskSorted
    - Refactor GetProfiles to use LINQ Select
    - Add localization strings for WinFsp missing dialog (en-US / zh-CN)
    - Code style: enforce braces on single-line if bodies, reformat XML doc comments

--- benchmarks/ManagedDrive.Benchmarks/ReadWriteBenchmarks.cs
+++ benchmarks/ManagedDrive.Benchmarks/ReadWriteBenchmarks.cs
@@ -8,18 +8,67 @@ namespace ManagedDrive.Benchmarks;
 [MinColumn, MaxColumn]
 public class ReadWriteBenchmarks
 {
-    private const string RamMountPoint = "R:";
     private const ulong CapacityBytes = 128 * 1024 * 1024;
+    private const string RamMountPoint = "R:";
+    private RamDisk _ramDisk = null!;
+
+    private string _ramFile = null!;
+
+    private byte[] _readBuffer = null!;
+
+    private string _tempDir = null!;
+
+    private string _tempFile = null!;
+
+    private byte[] _writeBuffer = null!;
 
     [Params(4 * 1024, 1 * 1024 * 1024, 64 * 1024 * 1024)]
-    public int FileSizeBytes { get; set; }
+    public int FileSizeBytes
+    {
+        get; set;
+    }
 
-    private RamDisk _ramDisk = null!;
-    private string _ramFile = null!;
-    private string _tempDir = null!;
-    private string _tempFile = null!;
-    private byte[] _writeBuffer = null!;
-    private byte[] _readBuffer = null!;
+    [GlobalCleanup]
+    public void Cleanup()
+    {
+        if (File.Exists(_ramFile))
+            File.Delete(_ramFile);
+        _ramDisk.Dispose();
+        if (Directory.Exists(_tempDir))
+            Directory.Delete(_tempDir, recursive: true);
+    }
+
+    [Benchmark(Description = "PhysicalDisk Read")]
+    public void PhysicalDisk_SequentialRead()
+    {
+        using var fs = new FileStream(_tempFile, FileMode.Open, FileAccess.Read,
+            FileShare.Read, bufferSize: 4096, FileOptions.SequentialScan);
+        fs.ReadExactly(_readBuffer, 0, _readBuffer.Length);
+    }
+
+    [Benchmark(Description = "PhysicalDisk Write", Baseline = true)]
+    public void PhysicalDisk_SequentialWrite()
+    {
+        using var fs = new FileStream(_tempFile, FileMode.Create, FileAccess.Write,
+            FileShare.None, bufferSize: 4096, FileOptions.WriteThrough);
+        fs.Write(_writeBuffer, 0, _writeBuffer.Length);
+    }
+
+    [Benchmark(Description = "RamDisk Read")]
+    public void RamDisk_SequentialRead()
+    {
+        using var fs = new FileStream(_ramFile, FileMode.Open, FileAccess.Read,
+            FileShare.Read, bufferSize: 4096, FileOptions.SequentialScan);
+        fs.ReadExactly(_readBuffer, 0, _readBuffer.Length);
+    }
+
+    [Benchmark(Description = "RamDisk Write")]
+    public void RamDisk_SequentialWrite()
+    {
+        using var fs = new FileStream(_ramFile, FileMode.Create, FileAccess.Write,
+            FileShare.None, bufferSize: 4096, FileOptions.WriteThrough);
+        fs.Write(_writeBuffer, 0, _writeBuffer.Length);
+    }
 
     [GlobalSetup]
     public void Setup()
@@ -43,47 +92,5 @@ public class ReadWriteBenchmarks
         // Pre-create files so read benchmarks can run in isolation
         File.WriteAllBytes(_ramFile, _writeBuffer);
         File.WriteAllBytes(_tempFile, _writeBuffer);
-    }
-
-    [GlobalCleanup]
-    public void Cleanup()
-    {
-        if (File.Exists(_ramFile))
-            File.Delete(_ramFile);
-        _ramDisk.Dispose();
-        if (Directory.Exists(_tempDir))
-            Directory.Delete(_tempDir, recursive: true);
-    }
-
-    [Benchmark(Description = "RamDisk Write")]
-    public void RamDisk_SequentialWrite()
-    {
-        using var fs = new FileStream(_ramFile, FileMode.Create, FileAccess.Write,
-            FileShare.None, bufferSize: 4096, FileOptions.WriteThrough);
-        fs.Write(_writeBuffer, 0, _writeBuffer.Length);
-    }
-
-    [Benchmark(Description = "RamDisk Read")]
-    public void RamDisk_SequentialRead()
-    {
-        using var fs = new FileStream(_ramFile, FileMode.Open, FileAccess.Read,
-            FileShare.Read, bufferSize: 4096, FileOptions.SequentialScan);
-        fs.ReadExactly(_readBuffer, 0, _readBuffer.Length);
-    }
-
-    [Benchmark(Description = "PhysicalDisk Write", Baseline = true)]
-    public void PhysicalDisk_SequentialWrite()
-    {
-        using var fs = new FileStream(_tempFile, FileMode.Create, FileAccess.Write,
-            FileShare.None, bufferSize: 4096, FileOptions.WriteThrough);
-        fs.Write(_writeBuffer, 0, _writeBuffer.Length);
-    }
-
-    [Benchmark(Description = "PhysicalDisk Read")]
-    public void PhysicalDisk_SequentialRead()
-    {
-        using var fs = new FileStream(_tempFile, FileMode.Open, FileAccess.Read,
-            FileShare.Read, bufferSize: 4096, FileOptions.SequentialScan);
-        fs.ReadExactly(_readBuffer, 0, _readBuffer.Length);
     }
 }
